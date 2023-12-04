e34f794f43862193ea08e05f25633e29a603c5c0
Kristinimum <kristinmaxwell43@gmail.com>
2023-12-04 13:10:57 +0000

    v34lab

--- plug-in-basedv11/terraform.tf
+++ plug-in-basedv11/terraform.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = ">= 1.0.0"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 3.0"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "3.1.0"
     }
   }
