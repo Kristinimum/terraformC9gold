38226b10887bf6da89f68cd7e7e8c039b9f96a9e
Kristinimum <kristinmaxwell43@gmail.com>
2023-12-07 05:17:38 +0000

    testingstep1wk22proj

--- asgtfproj22/main.tf
+++ asgtfproj22/main.tf
@@ -1,54 +1,25 @@
-resource "aws_placement_group" "woofers_group" {
-  name     = "test"
-  strategy = "cluster"
-}
-
-resource "aws_default_vpc" "default" {
-  default_vpc = "vpc-0ec51f31ec9b16cf1"
-  tags = {
-    Name = "Default VPC"
-  }
-}
-
-resource "aws_launch_template" "woofers_template" {
-  name_prefix   = "terraform-asg"
-  image_id      = "ami-0fa1ca9559f1892ec"
-  instance_type = "t2.micro"
+#Launch Template-needs to be created to make Auto Scaling Group. Needs a key pair.
+resource "aws_launch_template" "apache_template" {
+  name          = var.name
+  image_id           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
   user_data     = file("install_apache.sh")
-
+  tags = {
+    Name = "Woofers"
+  }
 }
 
-resource "aws_autoscaling_group" "woofers_asg" {
-  name                      = "woofers_asg_test"
-  max_size                  = 5
-  min_size                  = 2
-  health_check_grace_period = 300
-  #health_check_type         = "EC2"
-  desired_capacity    = 2
-  force_delete        = true
-  placement_group     = aws_placement_group.woofers_group.id
-  launch_template     = [aws_launch_template.woofers_template]
-  vpc_zone_identifier = ["$aws_subnet.main-public-1.id}", "$(aws_subnet.main-public-2.id}"]
-
-}
-
-
-
-resource "aws_security_group" "woofers_instance" {
-  name = "learn-asg-woofers-instance"
-  ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
-    #security_groups = [aws_security_group.terramino_lb.id]
+#Auto Scaling Group-must specify availibility zones
+resource "aws_autoscaling_group" "woofers_apache_asg" {
+  name               = var.name
+  availability_zones = var.availability_zones
+  desired_capacity   = 2
+  max_size           = 5
+  min_size           = 2
+  #need to specify what version of the template to use.
+  launch_template {
+    id      = aws_launch_template.apache_template.id
+    version = aws_launch_template.apache_template.latest_version
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  vpc_id = "vpc-0ec51f31ec9b16cf1"
 }
---
+++ asgtfproj22/variables.tf
@@ -0,0 +1,37 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+#instance variable
+variable "instance_type" {
+  description = "Instance type t2.micro"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "name" {
+  type    = string
+  default = "wooferswk22proj"
+}
+variable "ami_id" {
+  description = "Amazon Linux Instance"
+  type        = string
+  default     = "ami-0fa1ca9559f1892ec"
+}
+
+variable "key_name" {
+  description = "Key pair for this project"
+  type        = string
+  default     = "jenkins-kp"
+}
+
+variable "availability_zones" {
+  type    = list(string)
+  default = (["us-east-1c", "us-east-1d"])
+}
+
+variable "vpc_id" {
+  type    = string
+  default = "vpc-0ec51f31ec9b16cf1"
+}
