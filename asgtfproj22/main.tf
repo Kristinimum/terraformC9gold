f3a3f601d23e4a91a32884c32c92c0aa5e748220
Kristinimum <kristinmaxwell43@gmail.com>
2023-12-08 16:08:14 +0000

    sg and connected to launch template

--- asgtfproj22/main.tf
+++ asgtfproj22/main.tf
@@ -1,10 +1,11 @@
 #Launch Template-needs to be created to make Auto Scaling Group. Needs a key pair.
 resource "aws_launch_template" "apache_template" {
-  name          = var.name
-  image_id      = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  user_data     = filebase64("install_apache.sh")
+  name                   = var.name
+  image_id               = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  user_data              = filebase64("install_apache.sh")
+  vpc_security_group_ids = [aws_security_group.woofers_sg.id]
   tags = {
     Name = var.name
   }
@@ -12,11 +13,11 @@ resource "aws_launch_template" "apache_template" {
 
 #Auto Scaling Group-must specify availibility zones
 resource "aws_autoscaling_group" "woofers_apache_asg" {
-  name               = var.name
+  name                = var.name
   vpc_zone_identifier = var.availability_zones
-  desired_capacity   = 2
-  max_size           = 5
-  min_size           = 2
+  desired_capacity    = 2
+  max_size            = 5
+  min_size            = 2
   #need to specify what version of the template to use.
   launch_template {
     id      = aws_launch_template.apache_template.id
---
+++ asgtfproj22/securitygroup.tf
@@ -0,0 +1,28 @@
+#Security Group 
+resource "aws_security_group" "woofers_sg" {
+  name        = "woofers_sg"
+  description = "for web traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "HTTP access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "HTTPS access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
