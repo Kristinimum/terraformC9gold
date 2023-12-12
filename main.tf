7b05553aca1e7e95fd606096eb9ca46165f8027f
Kristinimum <kristinmaxwell43@gmail.com>
2023-12-12 12:48:28 +0000

    v61dynamicblockwith map

--- main.tf
+++ main.tf
@@ -398,29 +398,19 @@ resource "aws_security_group" "main" {
   vpc_id = aws_vpc.vpc.id
 
   dynamic "ingress" {
-    for_each = local.ingress_rules
+    for_each = var.web_ingress
     content {
       description = ingress.value.description
       from_port   = ingress.value.port
       to_port     = ingress.value.port
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+      protocol    = ingress.value.protocol
+      cidr_blocks = ingress.value.cidr_blocks
     }
   }
 }
 
 
-locals {
-  ingress_rules = [{
-    port        = 443
-    description = "Port 443"
-    },
-    {
-      port        = 80
-      description = "Port 80"
-    }
-  ]
-}
+
 
 
 
