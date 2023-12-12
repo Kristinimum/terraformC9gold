24c7860b5c6e51d0535b7430be9f4d0298517243
Kristinimum <kristinmaxwell43@gmail.com>
2023-12-12 12:42:16 +0000

    v61dynamic blocks with locals

--- main.tf
+++ main.tf
@@ -393,6 +393,36 @@ resource "aws_subnet" "list_subnet" {
   availability_zone = each.value.az
 }
 
+resource "aws_security_group" "main" {
+  name   = "core-sg"
+  vpc_id = aws_vpc.vpc.id
+
+  dynamic "ingress" {
+    for_each = local.ingress_rules
+    content {
+      description = ingress.value.description
+      from_port   = ingress.value.port
+      to_port     = ingress.value.port
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
+
+
+locals {
+  ingress_rules = [{
+    port        = 443
+    description = "Port 443"
+    },
+    {
+      port        = 80
+      description = "Port 80"
+    }
+  ]
+}
+
+
 
 
 
